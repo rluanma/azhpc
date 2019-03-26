6abd548e4d34db780c3a96a23eefcf1c9d92dfd6
Jithin Jose <jijos@microsoft.com>
2019-03-26 12:18:21 -0700

    test3

--- cn-setup.sh
+++ cn-setup.sh
@@ -3,27 +3,19 @@
 # fail on any error
 set -e
 
-HEADNODE=10.0.2.4
+HEADNODE=$1
+USER=$2
 
 sed -i 's/^ResourceDisk.MountPoint=\/mnt\/resource$/ResourceDisk.MountPoint=\/mnt\/local_resource/g' /etc/waagent.conf
 umount /mnt/resource
 
 mkdir -p /mnt/resource/scratch
 
-cat << EOF >> /etc/security/limits.conf
-*               hard    memlock         unlimited
-*               soft    memlock         unlimited
-*               soft    nofile          65535
-*               soft    nofile          65535
-EOF
-
 cat << EOF >> /etc/fstab
 $HEADNODE:/home    /home   nfs defaults 0 0
 $HEADNODE:/mnt/resource/scratch    /mnt/resource/scratch   nfs defaults 0 0
 EOF
 
-#yum --enablerepo=extras install -y -q epel-release
-#yum install -y -q nfs-utils htop pdsh psmisc
 until yum install -y -q nfs-utils
 do
     sleep 10
@@ -33,6 +25,4 @@ setsebool -P use_nfs_home_dirs 1
 mount -a
 
 # Don't require password for HPC user sudo
-echo "hpcuser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-# Disable tty requirement for sudo
-sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
--- hn-setup.sh
+++ hn-setup.sh
@@ -5,17 +5,8 @@ githubUser=$(echo "$scriptUri" | cut -d'/' -f4)
 githubRepo=$(echo "$scriptUri" | cut -d'/' -f5)
 githubBranch=$(echo "$scriptUri" | cut -d'/' -f6)
 
-USER=hpcuser
-
 IP=`ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
 localip=`echo $IP | cut --delimiter='.' -f -3`
-
-cat << EOF >> /etc/security/limits.conf
-*               hard    memlock         unlimited
-*               soft    memlock         unlimited
-*               hard    nofile          65535
-*               soft    nofile          65535
-EOF
 
 mkdir -p /mnt/resource/scratch
 chmod a+rwx /mnt/resource/scratch
@@ -25,6 +16,7 @@ yum install -y -q nfs-utils nmap htop pdsh screen git axel
 # need to update for git work
 yum update -y nss curl libcurl
 
+# Host NFS
 cat << EOF >> /etc/exports
 /home $localip.*(rw,sync,no_root_squash,no_all_squash)
 /mnt/resource/scratch $localip.*(rw,sync,no_root_squash,no_all_squash)
@@ -40,17 +32,16 @@ systemctl start nfs-lock
 systemctl start nfs-idmap
 systemctl restart nfs-server
 
-mkdir -p /home/$USER/bin
-chown $USER:$USER /home/$USER/bin
-
+USER=$2
 cat << EOF >> /home/$USER/.bashrc
-export WCOLL=/home/$USER/bin/hostlist
+export WCOLL=/home/$USER/bin/hostfile
 EOF
 chown $USER:$USER /home/$USER/.bashrc
 
-touch /home/hpcuser/bin/hostlist
-chown hpcuser:hpcuser /home/hpcuser/bin/hostlist
+touch /home/$USER/bin/hostfile
+chown $USER:$USER /home/$USER/bin/hostfile
 
+# Setup passwordless ssh to compute nodes
 ssh-keygen -f /home/$USER/.ssh/id_rsa -t rsa -N ''
 cat << EOF > /home/$USER/.ssh/config
 Host *
@@ -65,45 +56,13 @@ chown $USER:$USER /home/$USER/.ssh/*
 
 # Don't require password for HPC user sudo
 echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-# Disable tty requirement for sudo
-sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
 
-#
-# add .screenrc file
-#
-cat << EOF > /home/$USER/.screenrc
-screen -t "top"  0 top
-screen -t "bash 1"  0 bash
-
-defscrollback 10000
-
-sessionname local
-shelltitle bash
-startup_message off
-
-vbell off
-
-bind = resize =
-bind + resize +2
-bind - resize -2
-bind _ resize max
-
-caption always "%{= wr} $HOSTNAME %{= wk} %-Lw%{= wr}%n%f %t%{= wk}%+Lw %{= wr} %=%c %Y-%m-%d "
-
-zombie cr
-escape ^]]
-EOF
-chown $USER:$USER /home/$USER/.screenrc
-
-cd /home/$USER
-echo $1 > debug.log
-echo "git clone -b $githubBranch https://github.com/$githubUser/$githubRepo.git" >> debug.log
+# Add script for generating hostfile
+cd /tmp
+echo "git clone -b $githubBranch https://github.com/$githubUser/$githubRepo.git"
 git clone -b $githubBranch https://github.com/$githubUser/$githubRepo.git
-chown $USER:$USER -R azhpc
-chmod +x azhpc/scripts/*
-cd /home/$USER/bin
-for i in /home/$USER/azhpc/scripts/*; do
-	ln -s $i
-done
-
-rm -f install.py
+cd azhpc/scripts/
+mkdir -p /home/$USER/scripts
+cp -r * /home/$USER/scripts/
+chown $USER:$USER /home/$USER/scripts
+cd /tmp && rm -rf /tmp/*
